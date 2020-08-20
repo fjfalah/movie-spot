9176193f4bbac8f2fed2bd55a8fea2fe8d850b5d
falah <fajril.falah93@gmail.com>
2020-08-21 00:45:24 +0700

    Fix: Change to Secure API Request

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const IS_PRODUCTION = process.env.NODE_ENV === 'production';
-export const API_URL = process.env.API_URL || 'http://www.omdbapi.com';
+export const API_URL = process.env.API_URL || 'https://www.omdbapi.com';
 export const API_URL_KEY = process.env.API_URL_KEY || 'faf7e5bb';
