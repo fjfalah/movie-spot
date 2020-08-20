f91d860bdffec77ed0d0ece3f9147b2dfb9d037b
falah <fajril.falah93@gmail.com>
2020-08-20 13:41:06 +0700

    Feat: Create Initial Redux Config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,20 @@
 import { AppProps } from 'next/app';
 import React from 'react';
+import { Provider as ReduxProvider } from 'react-redux';
 import { ThemeProvider } from 'styled-components';
 
 import GlobalStyles from '../components/GlobalStyles';
+import { useStore } from '../store';
 import { theme } from '../themes';
 
 const AppRoot: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const store = useStore(pageProps.initialReduxState);
   return (
     <ThemeProvider theme={theme}>
-      <Component {...pageProps} />
-      <GlobalStyles />
+      <ReduxProvider store={store}>
+        <Component {...pageProps} />
+        <GlobalStyles />
+      </ReduxProvider>
     </ThemeProvider>
   );
 };
---
+++ src/store/index.ts
@@ -0,0 +1,56 @@
+import { useMemo } from 'react';
+import {
+  createStore,
+  applyMiddleware,
+  Store,
+  $CombinedState,
+  AnyAction,
+} from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+
+import reducers from './reducers';
+
+type ConfigureStoreType = Store<
+  {
+    readonly [$CombinedState]?: undefined;
+  },
+  AnyAction
+> & {
+  dispatch: unknown;
+};
+
+let store;
+const middlewares = [];
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(logger);
+}
+middlewares.push(thunk);
+const configureStore = (initialState = {}): ConfigureStoreType => {
+  return createStore(reducers, initialState, applyMiddleware(...middlewares));
+};
+export const initializeStore = (preloadedState = {}): ConfigureStoreType => {
+  let newStore = store ?? configureStore(preloadedState);
+
+  // After navigating to a page with an initial Redux state, merge that state
+  // with the current state in the store, and create a new store
+  if (preloadedState && store) {
+    newStore = configureStore({
+      ...store.getState(),
+      ...preloadedState,
+    });
+    // Reset the current store
+    store = undefined;
+  }
+
+  // For SSG and SSR always create a new store
+  if (typeof window === 'undefined') return newStore;
+  // Create the store once in the client
+  if (!store) store = newStore;
+
+  return newStore;
+};
+
+export const useStore = (initialState = {}): ConfigureStoreType => {
+  return useMemo(() => initializeStore(initialState), [initialState]);
+};
